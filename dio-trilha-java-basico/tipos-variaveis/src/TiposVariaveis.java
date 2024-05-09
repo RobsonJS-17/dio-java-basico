ca37283195c07cf7624adf6baba8131beb80e80b
RobsonJS-17 <robsonbravos@hotmail.com>
2024-05-09 10:51:20 -0300

    Exercicio criando smartTV

--- dio-trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
+++ dio-trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+
+               public static void main(String[] args) {
+                double resultado = (10*7) + (14/4);
+                String valorFinal = "O valor da operação: " + (resultado);
+                System.out.println(valorFinal);
+            }
+            
+        }
+    
